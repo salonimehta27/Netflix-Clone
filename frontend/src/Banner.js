79f490b00273f606b3025495c004aed8976772a3
Saloni <salonimehta2127@gmail.com>
2021-10-05 23:16:55 -0400

    fixed the div on the banner

--- frontend/src/Banner.js
+++ frontend/src/Banner.js
@@ -12,18 +12,20 @@ function Banner({ fetchUrl }) {
     }, [])
     console.log(movie)
     return (
-        <header className="banner" style={{
-            backgroundSize: "cover",
-            backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
-            backgroundPosition: "center center",
-        }}>
-            <div className="banner__contents"></div>
-            <h1 className="banner__title">{movie?.title || movie?.name || movie?.original_name}</h1>
-            <div className="banner__buttons">
-                <button className="banner__button">Play</button>
-                <button className="banner__button">My List</button>
+        <header className="banner"
+            style={{
+                backgroundSize: "cover",
+                backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+                backgroundPosition: "center center",
+            }}>
+            <div className="banner__contents">
+                <h1 className="banner__title">{movie?.title || movie?.name || movie?.original_name}</h1>
+                <div className="banner__buttons">
+                    <button className="banner__button">Play</button>
+                    <button className="banner__button">My List</button>
+                </div>
+                <h1 className="banner__description">{movie?.overview}</h1>
             </div>
-            <h1 className="banner__description">{movie?.overview}</h1>
         </header>
     )
 }
